b5a2e5e620607ea97700851dcd48396276096e7c
Stefan Marks <stefanmarks@users.noreply.github.com>
2025-03-21 17:08:05 +1300

    Adding 4 inputs per tracker

--- Assets/Scripts/IO_Device.cs
+++ Assets/Scripts/IO_Device.cs
@@ -5,21 +5,35 @@ using UnityEngine.InputSystem.XR;
 public class IO_Device : MonoBehaviour
 {
 	public InputActionProperty Input1;
+	public bool Input1On;
+	public InputActionProperty Input2;
+	public bool Input2On;
+	public InputActionProperty Input3;
+	public bool Input3On;
+	public InputActionProperty Input4;
+	public bool Input4On;
+
 	public InputActionProperty Output;
 
-	public bool InputOn;
+
 	public bool OutputOn;
 
 	void Start()
 	{
 		Input1.action?.Enable();
+		Input2.action?.Enable();
+		Input3.action?.Enable();
+		Input4.action?.Enable();
 		Output.action?.Enable();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		InputOn = Input1.action.inProgress;
+		Input1On = Input1.action.inProgress;
+		Input2On = Input2.action.inProgress;
+		Input3On = Input3.action.inProgress;
+		Input4On = Input4.action.inProgress;
 
 		if (OutputOn)
 		{
