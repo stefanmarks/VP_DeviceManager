5c52b47333facad3b975a5f543f6d57dfe019d88
Stefan Marks <stefanmarks@users.noreply.github.com>
2025-03-24 16:26:02 +1300

    Improvements
    - Adding HMD to tracked devices
    - Reducing traffic for IO
    - Updated SL OSC_Manager to avoid data replication

--- Assets/Scripts/OSC_Device.cs
+++ Assets/Scripts/OSC_Device.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class OSC_Device : MonoBehaviour
 {
 	public string Prefix = "/tracked_device";
+	public float  MinimumUpdateInterval = 1.0f;
 
 	public void Start()
 	{
@@ -16,6 +17,7 @@ public class OSC_Device : MonoBehaviour
 			m_io2 = new OSC_BoolVariable(Prefix + "/input2");
 			m_io3 = new OSC_BoolVariable(Prefix + "/input3");
 			m_io4 = new OSC_BoolVariable(Prefix + "/input4");
+			m_nextIoUpdate = 0;
 		}
 	}
 
@@ -28,10 +30,22 @@ public class OSC_Device : MonoBehaviour
 
 		if (m_io != null)
 		{
-			m_io1.Value = m_io.Input1On; m_io1.SendUpdate();
-			m_io2.Value = m_io.Input2On; m_io2.SendUpdate();
-			m_io3.Value = m_io.Input3On; m_io3.SendUpdate();
-			m_io4.Value = m_io.Input4On; m_io4.SendUpdate();
+			m_nextIoUpdate -= Time.deltaTime;
+			bool doUpdate = m_nextIoUpdate <= 0;
+			
+			if (m_io1.Value != m_io.Input1On) { m_io1.Value = m_io.Input1On; doUpdate = true; }
+			if (m_io2.Value != m_io.Input2On) { m_io2.Value = m_io.Input2On; doUpdate = true; }
+			if (m_io3.Value != m_io.Input3On) { m_io3.Value = m_io.Input3On; doUpdate = true; }
+			if (m_io4.Value != m_io.Input4On) { m_io4.Value = m_io.Input4On; doUpdate = true; }
+
+			if (doUpdate)
+			{
+				m_io1.SendUpdate();
+				m_io2.SendUpdate();
+				m_io3.SendUpdate();
+				m_io4.SendUpdate();
+				m_nextIoUpdate = MinimumUpdateInterval;
+			}
 		}
 	}
 
@@ -39,4 +53,5 @@ public class OSC_Device : MonoBehaviour
 	OSC_6DofPoseVariable m_pose;
 	IO_Device            m_io;
 	OSC_BoolVariable     m_io1, m_io2, m_io3, m_io4;
+	double               m_nextIoUpdate;
 }
