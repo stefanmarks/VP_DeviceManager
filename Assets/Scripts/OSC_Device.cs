8a25a5bcfe76d5ff24bb01915324e13fd08b54db
Stefan Marks <stefanmarks@users.noreply.github.com>
2025-03-24 15:15:48 +1300

    Adding streaming of IO
    - Also added trigger/grip as input 1/2 of controllers

--- Assets/Scripts/OSC_Device.cs
+++ Assets/Scripts/OSC_Device.cs
@@ -8,6 +8,15 @@ public class OSC_Device : MonoBehaviour
 	public void Start()
 	{
 		m_pose = new OSC_6DofPoseVariable(Prefix + "/pose", OSC_6DofPoseVariable.EDataFormat.Pos_RotQuat);
+		
+		m_io = GetComponent<IO_Device>();
+		if (m_io != null)
+		{
+			m_io1 = new OSC_BoolVariable(Prefix + "/input1");
+			m_io2 = new OSC_BoolVariable(Prefix + "/input2");
+			m_io3 = new OSC_BoolVariable(Prefix + "/input3");
+			m_io4 = new OSC_BoolVariable(Prefix + "/input4");
+		}
 	}
 
 	
@@ -16,8 +25,18 @@ public class OSC_Device : MonoBehaviour
 		m_pose.Position = transform.position;
 		m_pose.Rotation = transform.rotation;
 		m_pose.SendUpdate();
+
+		if (m_io != null)
+		{
+			m_io1.Value = m_io.Input1On; m_io1.SendUpdate();
+			m_io2.Value = m_io.Input2On; m_io2.SendUpdate();
+			m_io3.Value = m_io.Input3On; m_io3.SendUpdate();
+			m_io4.Value = m_io.Input4On; m_io4.SendUpdate();
+		}
 	}
 
 
 	OSC_6DofPoseVariable m_pose;
+	IO_Device            m_io;
+	OSC_BoolVariable     m_io1, m_io2, m_io3, m_io4;
 }
